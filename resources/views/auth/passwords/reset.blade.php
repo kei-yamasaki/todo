e807a57a6fef999ed560a5170a44debff9c85b19
kei-yamasaki <hello.tra.yamasaki@gmail.com>
2022-07-15 13:05:24 +0900

    add password reset token,validation

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -7,8 +7,18 @@
         <nav class="panel panel-default">
           <div class="panel-heading">パスワード再発行</div>
           <div class="panel-body">
+            {{-- パスワードリセット　バリデーションエラー表示 --}}
+            @if ($errors->any())
+              <div class="alert alert-danger" role="alert">
+                @foreach($errors->all() as $message)
+                  <p>{{ $message }}</p>
+                @endforeach
+              </div>
+            @endif
             <form action="{{ route('password.update') }}" method="POST">
               @csrf
+              {{-- パスワードリセットトークンの送信 --}}
+              <input type="hidden" name="token" value="{{ $token }}" />
               <div class="form-group">
                 <label for="email">メールアドレス</label>
                 <input type="text" class="form-control" id="email" name="email" />
